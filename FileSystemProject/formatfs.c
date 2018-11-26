faccb4e46d1a60ba7dacb69c07bc0311b42f35c5
Thomas Hill <thomashill1993@me.com>
2018-11-26 12:15:17 -0600

    added formats

---
+++ FileSystemProject/formatfs.c
@@ -0,0 +1,16 @@
+//
+//  formatfs.c
+//  FileSystemProject
+//
+//  Created by Thomas Hill on 11/26/18.
+//  Copyright © 2018 Thomas Hill. All rights reserved.
+//
+
+#include <stdio.h>
+#include "softwaredisk.c"
+#include "formatfs.h"
+
+void formatfs(void)
+{
+    init_software_disk();
+}
---
+++ FileSystemProject/formatfs.h
@@ -0,0 +1,14 @@
+//
+//  formatfs.h
+//  FileSystemProject
+//
+//  Created by Thomas Hill on 11/26/18.
+//  Copyright © 2018 Thomas Hill. All rights reserved.
+//
+
+#ifndef formatfs_h
+#define formatfs_h
+
+void formatfs(void);
+
+#endif /* formatfs_h */
